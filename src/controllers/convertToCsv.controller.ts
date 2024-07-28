f27f079cec028c938317dd2151d0f44e10aa70ec
luisaramicar11 <luisaf.ramirez.cardona11@gmail.com>
2024-07-27 20:03:29 -0500

    Adding structure to index.html

--- src/controllers/convertToCsv.controller.ts
+++ src/controllers/convertToCsv.controller.ts
@@ -1,5 +1,6 @@
 import {DataTable, ColumnName} from "../models/file.model"
 
+/*Adding function to transform array information into plain text file*/
 export async function convertToCsvController(data: DataTable, columnNames: ColumnName): Promise<string>{
     const csvContent: string = columnNames.join(",") + "\n" + data.map(row=>columnNames.map(colName=>row[colName]).join(",")).join("\n");
     return csvContent;  

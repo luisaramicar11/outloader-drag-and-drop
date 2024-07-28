60ae1ee0b5acc7c5355da31a5b3f54d85e14b4f2
luisaramicar11 <luisaf.ramirez.cardona11@gmail.com>
2024-07-27 21:25:56 -0500

    adding index.ts

--- src/controllers/file.controller.ts
+++ src/controllers/file.controller.ts
@@ -3,22 +3,30 @@ import { IDataRow,  DataTable, ColumnName} from "../models/file.model"
 /* class that process information from file */
 export class FileController {
     public fileContent: string;
+    public data: DataTable = [];
+    public columnName: ColumnName = [];
     constructor(fileContent: string) {
         this.fileContent = fileContent;
     }
     public processFile(): void {
         const lines: string[] = this.fileContent.split(/[\r\n]+/).filter(line => line.trim()!=="");
         if(lines.length > 0){
-            const columnNames: ColumnName = lines[0].split(",");
-            const data: DataTable = lines.slice(1).map(line=>{
+            this.columnName = lines[0].split(",");
+            this.data = lines.slice(1).map(line=>{
                 const value: string[] = line.split(',');
                 const row: IDataRow = {};
-                columnNames.forEach((colName, index)=>{
+                this.columnName.forEach((colName, index)=>{
                     row[colName] = value[index] || "";
                 })
                 return row;
             })
         }
+    }
+    public getColumnNames(): ColumnName{
+        return this.columnName;
+    }
+    public getData(): DataTable{
+        return this.data;
     }
 }
 
--- src/controllers/filter.controller.ts
+++ src/controllers/filter.controller.ts
@@ -2,6 +2,7 @@ import { DataTable } from "../models/file.model";
 
 export function filterDataController(data: DataTable, searchTerm: string): DataTable{
     if(!searchTerm) return data;
+    console.log(searchTerm);
     searchTerm = searchTerm.toLowerCase();
     /* Returns the information that matches with search term*/
     return data.filter(row=>Object.values(row).some(value=>value.toLowerCase().includes(searchTerm)));
